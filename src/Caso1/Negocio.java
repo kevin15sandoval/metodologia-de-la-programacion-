42b4467977ad94052194d271dbc0e39e7b7381fe
kevin15sandoval <kevin15sandoval@gmail.com>
2023-02-23 18:06:54 +0100

    - intro enunciado

--- src/Caso1/Negocio.java
+++ src/Caso1/Negocio.java
@@ -1,5 +1,16 @@
 package Caso1;
 
+/**
+ * caso de estudio 1 
+ * 
+ * - se neceistan calcular los numeros hexagonales, Los primeros números hexagonales son:
+1, 6, 15, 28, 45, 66, 91, 120, 153, 190, 231, 276, 325, 378, 435, 496, 561, 630, 703, 780, 861, 946
+ * 
+ * @author 34602
+ *
+ */
+
 public class Negocio {
+
 
 }
