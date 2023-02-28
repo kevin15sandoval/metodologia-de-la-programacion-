d91ba61e56e3a38c4d6dfc34aa21f2e7e1fe15dc
kevin15sandoval <kevin15sandoval@gmail.com>
2023-02-28 14:05:52 +0100

    caso recursivo hecho
    el caso recursivo del algoritmo esta hecho pro si quieren darle un vistazo

--- src/Caso1/Recursivo.java
+++ src/Caso1/Recursivo.java
@@ -1,14 +1,20 @@
 package Caso1;
 /**
- * <h1>Aplicación del método recursivo.</h1>
+ * <h1>Aplicación del método recursivo.</h1>ç
  * @author Kevin A.
- *
+ * paso: 1: establecemos el caso base que sera cuando el valor de la entrada es 1, por lo que el return sera 1 ya que es el caso base 
+ * paso 2: estabelcemos el metrodo recursivo aplicando la formula del metodo iteratico el cuañl es 4*n+1 y luego hacemos la recursividad llamando
+ * al metodo nuevamente y restandole uno para asi llegar nuevamnete el caso base y nos el valor correspondiente del numero hexagonal.
  */
 public class Recursivo {
+	
 	public static int recursivo(int valorEntrada) {
-		
-		System.out.println("Dentro del método recursivo");
-		return valorEntrada;
+		 //caso base   
+		if (valorEntrada == 1) {
+		        return 1;
+		    }
+		 //caso recursivo    
+		 return 4 * valorEntrada + 1 + recursivo(valorEntrada - 1);
 	}
-
+	
 }
