cc5a2ad054427d65a20115d6da39213e0cf44f8a
gmnpjpn <germanpajareropino@gmail.com>
2023-03-01 13:56:14 +0100

    Anadido Caso Iterativo

--- src/Caso1/Iteracion.java
+++ src/Caso1/Iteracion.java
@@ -8,6 +8,20 @@ public class Iteracion {
 	public static int iteracion(int valorEntrada) {
 		
 		System.out.println("Dentro de la opcion de iteracion.");
-		return valorEntrada;
+
+		System.out.println("*****************************************************************");
+        System.out.println("Este programa calcula un número hexagonal de forma iterativa:");
+        System.out.println("*****************************************************************\n");
+
+        int acumulador = 0;	//Acumula los resultados del sumatorio
+        int resultado;		//Contendra el resultado final
+
+        for (int i = 0 ; i <= (valorEntrada-1); i++) {
+            acumulador += (4 * i) + 1;
+        }
+
+        resultado = acumulador;
+
+        System.out.printf("El numero hexagonal con indice %d es %d.\n", valorEntrada, resultado);
 	}
 }
