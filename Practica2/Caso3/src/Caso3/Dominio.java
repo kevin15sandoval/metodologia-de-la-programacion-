343a951cb6de4b8ba2f9de6199b14f2a4a571f5c
kevin15sandoval <kevin15sandoval@gmail.com>
2023-03-30 18:09:19 +0200

    Practica2-Caso3-Voraz

---
+++ Practica2/Caso3/src/Caso3/Dominio.java
@@ -0,0 +1,5 @@
+package Caso3;
+
+public class Dominio {
+
+}
---
+++ Practica2/Caso3/src/Caso3/Presentacion.java
@@ -0,0 +1,10 @@
+package Caso3;
+
+public class Presentacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
